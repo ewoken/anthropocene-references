a6f067e02b4ee41c2d18a9eb01090e10ad2620aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-09 13:00:42 +0000

    Deploy ewoken/anthropocene-references to github.com/ewoken/anthropocene-references.git:gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# Anthropocene references
---
+++ precache-manifest.3b21efb8c8f48101768afc23f944475e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2bcc7004960a0cb812",
+    "url": "/anthropocene-references/static/js/runtime~main.ce2bcc70.js"
+  },
+  {
+    "revision": "c21d87d1a25e18c218b9",
+    "url": "/anthropocene-references/static/js/main.c21d87d1.chunk.js"
+  },
+  {
+    "revision": "98484b2552fd6ad3d819",
+    "url": "/anthropocene-references/static/js/1.98484b25.chunk.js"
+  },
+  {
+    "revision": "c21d87d1a25e18c218b9",
+    "url": "/anthropocene-references/static/css/main.f469e98f.chunk.css"
+  },
+  {
+    "revision": "98484b2552fd6ad3d819",
+    "url": "/anthropocene-references/static/css/1.db41f7b4.chunk.css"
+  },
+  {
+    "revision": "759b3143146485a6870f6423a59ceacf",
+    "url": "/anthropocene-references/index.html"
+  }
+];
