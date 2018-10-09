e113e43ec16a625885307385c6b9b66f9becd27c
ewoken <ewokenfr@gmail.com>
2018-10-09 11:24:01 +0200

    Updates

---
+++ precache-manifest.ad8470f11520dd2ab24f65efb7ca4cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2bcc7004960a0cb812",
+    "url": "/anthropocene-references/static/js/runtime~main.ce2bcc70.js"
+  },
+  {
+    "revision": "ee3c51f0e5ea4fbd1fd1",
+    "url": "/anthropocene-references/static/js/main.ee3c51f0.chunk.js"
+  },
+  {
+    "revision": "23b73be940e55d43b5d0",
+    "url": "/anthropocene-references/static/js/1.23b73be9.chunk.js"
+  },
+  {
+    "revision": "ee3c51f0e5ea4fbd1fd1",
+    "url": "/anthropocene-references/static/css/main.5360989f.chunk.css"
+  },
+  {
+    "revision": "23b73be940e55d43b5d0",
+    "url": "/anthropocene-references/static/css/1.a9a9e2e6.chunk.css"
+  },
+  {
+    "revision": "df56490b26c2ba3bbcc3f5dec117546e",
+    "url": "/anthropocene-references/index.html"
+  }
+];
