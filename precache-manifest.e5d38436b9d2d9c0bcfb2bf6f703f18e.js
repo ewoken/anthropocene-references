881fb9207615c7b3ace75f709ac00ed71fb234af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-12 08:00:23 +0000

    Deploy ewoken/anthropocene-references to github.com/ewoken/anthropocene-references.git:gh-pages

---
+++ precache-manifest.e5d38436b9d2d9c0bcfb2bf6f703f18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2bcc7004960a0cb812",
+    "url": "/anthropocene-references/static/js/runtime~main.ce2bcc70.js"
+  },
+  {
+    "revision": "7b878ffdf90b1e8d29ea",
+    "url": "/anthropocene-references/static/js/main.7b878ffd.chunk.js"
+  },
+  {
+    "revision": "242c7455d48c3a6b4270",
+    "url": "/anthropocene-references/static/js/1.242c7455.chunk.js"
+  },
+  {
+    "revision": "7b878ffdf90b1e8d29ea",
+    "url": "/anthropocene-references/static/css/main.ddea8a5f.chunk.css"
+  },
+  {
+    "revision": "242c7455d48c3a6b4270",
+    "url": "/anthropocene-references/static/css/1.b3a4b0be.chunk.css"
+  },
+  {
+    "revision": "75b68287c9f4deab83d729a8725b8b90",
+    "url": "/anthropocene-references/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/anthropocene-references/precache-manifest.d41e5f698cca1c99e9ff58ea246dcad9.js"
+  "/anthropocene-references/precache-manifest.e5d38436b9d2d9c0bcfb2bf6f703f18e.js"
 );
 
 workbox.clientsClaim();
